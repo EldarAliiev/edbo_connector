d53e1a8d383d809df7e8a99c6a891a821bf5bc09
Eldar Aliiev <bc.zim.green@gmail.com>
2017-08-05 15:17:38 +0300

    Setup improved

--- setup.py
+++ setup.py
@@ -1,39 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
-from edbo_connector import (
-    __name__ as package_name,
-    __version__ as package_version,
-    __license__ as package_license,
-    __author__ as package_author,
-    __email__ as package_author_email
-)
+from setuptools import setup, find_packages
+import edbo_connector
 
 setup(
-    name=package_name,
-    version=package_version,
-    author=package_author,
-    author_email=package_author_email,
+    name=edbo_connector.__name__,
+    version=edbo_connector.__version__,
+    author=edbo_connector.__author__,
+    author_email=edbo_connector.__email__,
     url='https://github.com/ZimGreen/edbo-connector-py',
     download_url='https://github.com/ZimGreen/edbo-connector-py/archive/master.zip',
-    license=package_license,
+    license=edbo_connector.__license__,
     description='edbo_connector',
     long_description=open('README.rst').read(),
-    packages=[
-        'edbo_connector',
-    ],
+    packages=find_packages(),
     install_requires=[
         'requests==2.18.3'
     ],
     classifiers=[
         'Development Status :: 5 - Production/Stable'
-        'License :: OSI Approved :: %s License' % package_license,
+        'License :: OSI Approved :: %s License' % edbo_connector.__license__,
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
@@ -46,5 +36,7 @@ setup(
         'RESTful API',
         'EDBO client',
     ],
-    zip_safe=False
+    test_suite='tests',
+    zip_safe=False,
+    include_package_data=True
 )
